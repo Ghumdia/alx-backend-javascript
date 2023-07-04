fc076105abda93accf9fed9d51595f34681a92ea
Your Name <you@example.com>
2023-07-04 18:03:38 +0000

    Committed

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,14 @@
 function handleResponseFromAPI(promise) {
-	let p = new Promise((resolve, reject) => {
-		if (promise) {
-			resolve({ status: 200, body: 'Success' });
-		} else {
-			reject(new Error(''));
-		}
-	});
-  p.then(() => {
-	  console.log('Got a response from the API');
+  const p = new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error(''));
+    }
   });
-
+  p.then(() => {
+    console.log('Got a response from the API');
+  });
 }
 
 export default handleResponseFromAPI;
