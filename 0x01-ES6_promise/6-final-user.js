c1b852f67c2d3a64b1192d6e1dbf8399435c86aa
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2023-07-05 16:15:47 +0100

    committed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,22 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-const handleProfileSignup = async (firstName, lastName, fileName) => {
-  const promises = [];
-
-  promises.push(
-    signUpUser(firstName, lastName)
-      .then((user) => ({ status: 'fulfilled', value: user }))
-      .catch((error) => ({ status: 'rejected', value: error })),
-  );
-
-  promises.push(
-    uploadPhoto(fileName)
-      .then((result) => ({ status: 'fulfilled', value: result }))
-      .catch((error) => ({ status: 'rejected', value: error })),
-  );
-
-  return Promise.all(promises);
-};
-
-export default handleProfileSignup;
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
+}
